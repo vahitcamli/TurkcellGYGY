a846203665fc6e6def7300f573a23faf4b1e0d4b
vahitcamli <camlivahit@gmail.com>
2024-03-23 16:59:35 +0300

    Pair-2

---
+++ rentaCarProject/src/main/java/com/turkcell/rentaCarProject/RentaCarProjectApplication.java
@@ -0,0 +1,13 @@
+package com.turkcell.rentaCarProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaCarProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaCarProjectApplication.class, args);
+	}
+
+}
---
+++ rentaCarProject/src/test/java/com/turkcell/rentaCarProject/RentaCarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.rentaCarProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaCarProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
